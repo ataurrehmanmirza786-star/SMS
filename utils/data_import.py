7990950d1b16189e00ac01ff30a80d0799667fa3
ataurrehmanmirza786-star <ataurrehmanmirza.786@gmail.com>
2025-10-29 12:37:32 +0500

    Update data_import.py

--- utils/data_import.py
+++ utils/data_import.py
@@ -1,1 +1,18 @@
+import pandas as pd
+from controllers.address_controller import AddressController
 
+def import_addresses_from_csv(file_path, controller):
+    """Import addresses from a CSV file."""
+    df = pd.read_csv(file_path)
+    
+    for _, row in df.iterrows():
+        address_data = {
+            'category': row['category'],
+            'number': str(row['number']),
+            'row': str(row['row']),
+            'block': row['block'],
+            'total_floors': int(row['total_floors'])
+        }
+        controller.add_address(address_data)
+    
+    return True
