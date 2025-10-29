4c752cab39aa95e344f18d112c5f53073ef5eb24
ataurrehmanmirza786-star <ataurrehmanmirza.786@gmail.com>
2025-10-29 12:47:35 +0500

    Update financial_management.py

--- ui/financial_management.py
+++ ui/financial_management.py
@@ -1,1 +1,16 @@
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
 
+class FinancialManagementWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        layout = QVBoxLayout(self)
+        
+        title = QLabel("Financial Management")
+        title.setStyleSheet("font-size: 18px; font-weight: bold; margin: 10px;")
+        layout.addWidget(title)
+        
+        content = QLabel("Financial Management module will be implemented here.")
+        layout.addWidget(content)
